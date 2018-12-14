fdd2070ca461272b9f602e4ca2e3b716542e425c
Liam Owen <l1amow3n5@yahoo.com>
2018-12-14 12:48:18 -0700

    added the updateView method and the loadURL and loadPDF

---
+++ InternetProject/Controller/InternetDetailViewController.swift
@@ -0,0 +1,81 @@
+//
+//  InternetDetailViewController.swift
+//  InternetProject
+//
+//  Created by Owen, Liam on 12/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public class InternetDetailViewController: UIViewController
+{
+    
+    @IBOutlet weak var webViewer: WKWebView!
+    @IBOutlet weak var screenTitle: UILabel!
+    
+    var detailTitle : String?
+    {
+        didSet
+        {
+            updateView()
+        }
+    }
+    
+    var detailAddress : String?
+    {
+        didSet
+        {
+            updateView()
+        }
+    }
+    
+    private func updateView() -> Void
+    {
+        if (detailTitle != nil && screenTitle != nil && webViewer != nil)
+        {
+            if (detailTitle?.range(of: "Definitions", options: .caseInsensitive) != nil)
+            {
+                loadPDF()
+            }
+            else
+            {
+                if (detailAddress != nil)
+                {
+                    loadURL(webAddress: detailAddress!)
+                }
+            }
+            screenTitle?.text = detailTitle
+        }
+    }
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func loadURL(webAddress: String) -> Void
+    {
+        
+    }
+    
+    private func loadPDF() -> Void
+    {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
